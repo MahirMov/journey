165557eb861fe243f4f507e601531063a3c8c056
root <root@DESKTOP-RHL201E.localdomain>
2026-06-10 13:05:21 +0400

    updating deploy.sh to work with docker compose

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,6 @@
-
 #!/bin/bash
 
-docker stop my-server 2>/dev/null
-docker rm my-server 2>/dev/null
-
-docker build -t my-server .
-
-docker run -d -p 80:80 --name my-server my-server
+docker compose down
+docker compose up -d --build
 
 echo "deployed!"
